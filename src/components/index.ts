f5b8d058dd3ef5196009b813dbd6bfa1001413f0
Pedrojok01 <pedrojok@pm.me>
2025-03-24 13:48:14 +0700

    Fix husky script in package.json

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,5 @@ export * from "./AddressInput";
 export * from "./DarkModeButton";
 export * from "./Footer";
 export * from "./Header";
-export * from "./MainPane";
 export * from "./InfoText";
+export * from "./MainPane";
